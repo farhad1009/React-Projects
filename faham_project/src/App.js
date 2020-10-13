95d44c4794a70cbbb678c01f16bea45999002681
farhad1009 <farhad1009@gmail.com>
2020-10-13 08:20:46 +0600

    Repository updated on 13 October, 2020

--- faham_project/src/App.js
+++ faham_project/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SiteMap from './component/site_map';
+import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- faham_project/src/component/home.jsx
+++ faham_project/src/component/home.jsx
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {Button, Accordion, Card} from 'react-bootstrap'
 
 
 class Home extends Component{
 
     render(){
         return(
-            <div>
+            <div className="container">
+                <Button variant="outline-primary">Primary</Button>
                 <h1>Home</h1>
+
+                <Accordion defaultActiveKey="0">
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+        Click me!
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="0">
+      <Card.Body>Hello! I'm the body</Card.Body>
+    </Accordion.Collapse>
+  </Card>
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="link" eventKey="1">
+        Click me!
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="1">
+      <Card.Body>Hello! I'm another body</Card.Body>
+    </Accordion.Collapse>
+  </Card>
+</Accordion>
+
+
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
